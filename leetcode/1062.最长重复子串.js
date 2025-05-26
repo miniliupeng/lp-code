6697b7861489c5d9bc6c37a5438f59c5cb4af3f3
刘鹏 <2254842466@qq.com>
2025-05-26 10:32:30 +0800

    feat

--- leetcode/1062.最长重复子串.js
+++ leetcode/1062.最长重复子串.js
@@ -76,6 +76,29 @@ var longestRepeatingSubstring = function (s) {
 // @lc code=end
 
 
+// # 最长重复子串解题思路
+
+// 这道题使用二分查找解决，主要思路如下：
+
+// 1. **问题定义**：找出字符串中最长的重复子串长度
+
+// 2. **二分查找思路**：
+//    - 如果存在长度为L的重复子串，那么一定存在长度为1到L-1的重复子串
+//    - 如果不存在长度为L的重复子串，那么一定不存在长度大于L的重复子串
+//    - 因此可以用二分查找确定最大长度
+
+// 3. **算法步骤**：
+//    - 定义`search(len)`函数检查是否存在长度为len的重复子串
+//    - 用Set记录已见过的子串
+//    - 使用二分查找，初始区间为[0, n]
+//    - 当找到长度为mid的重复子串时，在[mid+1, right]中继续查找
+//    - 当没找到长度为mid的重复子串时，在[left, mid-1]中查找
+//    - 最终返回left-1，即最后一个返回true的长度
+
+// 4. **时间复杂度**：O(n² log n)，其中n是字符串长度
+
+// 这种方法高效地找到了最长重复子串的长度。
+
 
 // console.log(longestRepeatingSubstring('abbaba'));
 // console.log(longestRepeatingSubstring('aabcaabdaab'));
