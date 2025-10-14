e915f69408f815bf01c7fc1cb303333ff8944ac4
刘鹏 <2254842466@qq.com>
2025-10-14 18:57:38 +0800

    feat

--- ts/index.ts
+++ ts/index.ts
@@ -22,18 +22,14 @@
 // |
 // ?
 
-
-
 // 模板字符串类型
 // 实现字面量类型的拼接
 // 自动分发: 当一个模板字符串类型中的插槽传入了联合类型时，这个模板字符串类型会自动被扩展为使用所有联合类型的组合
 
-
-
 type Func = (...args: any[]) => any;
 
-type FunctionReturnType<T extends Func> = T extends (
-  ...args: any[]
-) => infer R
-  ? R
-  : never;
+type FunctionReturnType<T extends Func> = T extends (...args: any[]) => infer R ? R : never;
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
